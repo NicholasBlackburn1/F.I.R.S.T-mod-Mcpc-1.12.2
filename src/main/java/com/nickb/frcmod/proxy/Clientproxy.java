b445bebf69545f8e450eef80b042003a59aef98d
nickblackburn1 <nickblackburn02@gmail.com>
2019-04-30 12:33:44 -0400

    got proxy working

--- src/main/java/com/nickb/frcmod/proxy/Clientproxy.java
+++ src/main/java/com/nickb/frcmod/proxy/Clientproxy.java
@@ -1,5 +1,16 @@
 package com.nickb.frcmod.proxy;
 
-public class Clientproxy{
-    
+import com.nickb.frcmod.main;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class Clientproxy extends Commonproxy{
+
+    public void registerItemRenderer(Item item, int meta, String id) {
+	ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(main.modId + ":" + id, "inventory"));
+  }
 }
--- src/main/java/com/nickb/frcmod/proxy/Serverproxy.java
+++ src/main/java/com/nickb/frcmod/proxy/Serverproxy.java
@@ -1,5 +1,22 @@
 package com.nickb.frcmod.proxy;
 
-public class Serverproxy{
-    
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class Serverproxy {
+    @SubscribeEvent
+    void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    @SubscribeEvent
+    void Init(FMLInitializationEvent event) {
+
+    }
+    @SubscribeEvent
+    void postInit(FMLPostInitializationEvent event){
+
+    }
+
 }
