5f885a7beba8132e2236f9638035952b1aeb8c09
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-05-13 09:54:09 -0400

    fixed loading

--- build/sources/main/java/com/nickb/frcmod/Enitity/RegisterEntity.java
+++ build/sources/main/java/com/nickb/frcmod/Enitity/RegisterEntity.java
@@ -8,6 +8,7 @@ import com.nickb.frcmod.main;
 import com.nickb.frcmod.Enitity.Dean.Dean;
 import com.nickb.frcmod.Enitity.Dean.RenderDean;
 import com.nickb.frcmod.Enitity.Robot.RenderRobot;
+import com.nickb.frcmod.Enitity.Robot.entityRobot;
 
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Biomes;
@@ -26,15 +27,16 @@ public class RegisterEntity {
         // Every entity in our mod has an ID (local to this mod)
 
         int id = 1;
+        int id2= 3;
         EntityRegistry.registerModEntity(dean, Dean.class, "Dean_kamen", id++, main.instance, 64, 3, true, 0x996600,
                 0x00ff00);
-        /*EntityRegistry.registerModEntity(robot, com.nickb.frcmod.Enitity.Robot.robot.class, "frcbot", id++, main.instance, 64, 3, true, 0x996600,
-                0x00ff00);*/
+        EntityRegistry.registerModEntity(robot, entityRobot.class, "frcbot", id2++, main.instance, 64, 3, true, 0x996000,
+                0x00ffe0);
         // We want our mob to spawn in Plains and ice plains biomes. If you don't add
         // this then it will not spawn automatically
         // but you can of course still make it spawn manually
         EntityRegistry.addSpawn(Dean.class, 100, 3, 5, EnumCreatureType.MONSTER, Biomes.PLAINS, Biomes.ICE_PLAINS);
-
+        
         // This is the loot table for our mob
         LootTableList.register(Dean.LOOT);
     }
@@ -42,7 +44,6 @@ public class RegisterEntity {
     @SideOnly(Side.CLIENT)
     public static void initModels() {
         RenderingRegistry.registerEntityRenderingHandler(Dean.class, RenderDean.FACTORY);
-       /* RenderingRegistry.registerEntityRenderingHandler(com.nickb.frcmod.Enitity.Robot.robot.class,
-                RenderRobot.FACTORY);*/
+        RenderingRegistry.registerEntityRenderingHandler(entityRobot.class, RenderRobot.FACTORY);
         }
 }
